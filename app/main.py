4967745dc7e0d0851b3801a8900f0c598799faeb
Andres <andresacg30@gmail.com>
2022-08-08 09:54:23 -0400

    upload files

--- app/main.py
+++ app/main.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from app.script.emails import send_email
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
+UPLOAD_FOLDER = os.fsdecode("app/script/Enviar")
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['pdf'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/")
-
-
 def index():
     return render_template("index.html")
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/')
+        else:
+            flash('Your file must be a PDF')
+            return redirect(request.url)
 
 @app.route("/send", methods=["GET", "POST"])
 def send():
